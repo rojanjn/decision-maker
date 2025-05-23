d418f3f99df5e00921cec6f1dba83df7ba92a27d
Rojan Jafarnezhad <101561560@georgebrown.ca>
2025-05-23 19:10:33 +0330

    js and html modified

---
+++ script.js
@@ -0,0 +1,14 @@
+const answers = [
+    "Yes.",
+    "No.",
+    "Ask again later.",
+    "Definitely.",
+    "I wouldn't count on it.",
+    "Maybe...",
+    "Absolutely not.",
+    "Without a doubt.",
+    "That's a tricky question...",
+    "Ooh, I don't know about that.",
+    "Go for it.",
+    "Don't hesitate on it."
+]
